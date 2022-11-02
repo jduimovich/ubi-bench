bc045af5b87102f7b436790c1a8977e044b7dec2
John Duimovich <jduimovich@gmail.com>
2022-11-02 10:33:00 -0400

    refactor

--- run.sh
+++ run.sh
@@ -1,44 +1,35 @@
 #!/usr/bin/env bash
+  
+function build () { 
+    UBI=$1
+    docker build -t ubi-standard.ubi$UBI  --build-arg VER=$UBI --build-arg SIZE= .
+    docker build -t ubi-standard-minimal.ubi$UBI  --build-arg VER=$UBI --build-arg SIZE=-minimal .
+}
 
-echo "Building Images"
+function bench () { 
+    UBI=$1
+    echo "Running ubi-standard.ubi$UBI" 
+    for i in {1..5}
+    do 
+        time docker run --rm ubi-standard.ubi$UBI
+    done  
+    echo "Running ubi-standard-minimal.ubi$UBI" 
+    for i in {1..5}
+    do 
+        time docker run --rm ubi-standard-minimal.ubi$UBI
+    done 
+}
 
-docker build -t ubi-standard.ubi7  --build-arg VER=7 --build-arg SIZE= .
-docker build -t ubi-standard-minimal.ubi7  --build-arg VER=7 --build-arg SIZE=-minimal .
-
-docker build -t ubi-standard.ubi8  --build-arg VER=8 --build-arg SIZE= .
-docker build -t ubi-standard-minimal.ubi8  --build-arg VER=8 --build-arg SIZE=-minimal .
- 
+build 7
+build 8
+build 9   
 echo "Sizes of Images"
-
-docker images | grep ubi-standard 
- 
-echo "Running ubi-standard.ubi7"
-
-for i in {1..5}
-do 
-time docker run --rm ubi-standard.ubi7
-done
- 
-echo "Running ubi-standard-minimal.ubi7"
-
-for i in {1..5}
-do 
-time docker run --rm ubi-standard-minimal.ubi7
-done
-
-echo "Running ubi-standard.ubi8" 
-for i in {1..5}
-do 
-time docker run --rm ubi-standard.ubi8
-done
-
-echo "Running ubi-standard-minimal.ubi8" 
-for i in {1..5}
-do 
-time docker run --rm ubi-standard-minimal.ubi7
-done
- 
- echo "Running alpine" 
+docker images | grep ubi-standard  
+bench 7
+bench 8
+bench 9   
+  
+echo "Baseline agains alpine" 
 for i in {1..5}
 do 
 time docker run --rm alpine uname 
